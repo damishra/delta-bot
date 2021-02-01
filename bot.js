4c1db4b06e3bcbfdf88a622cd11aea0be73787a8
damishra <dxm2269@rit.edu>
2021-02-01 18:49:29 -0500

    sec: switched config file for env

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-const Discord = require('discord.js')
-const config = require('./config')
-const { title } = require('process')
-const discordClient = new Discord.Client()
+const Discord = require('discord.js');
+const { title } = require('process');
+const discordClient = new Discord.Client();
 
 discordClient.on('ready', () => {
-  console.log(`Logged in as ${discordClient.user.tag}!`)
-})
-discordClient.login(config.discordApiToken)
+  console.log(`Logged in as ${discordClient.user.tag}!`);
+});
+
+discordClient.login(process.env.DISCORD_API_TOKEN);
